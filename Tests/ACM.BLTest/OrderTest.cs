07097a20bdd52b9551d9ffe756b7cfd00d14bf4f
Momo <chraiet009@outlook.com>
2020-01-12 20:53:06 -0500

    Added OrderTest

---
+++ Tests/ACM.BLTest/OrderTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestMethod]
+        public void ValidateOrderDateNull()
+        {
+            //  Arrange
+            var order = new Order
+            {
+                OrderDate = null
+            };
+            bool expected = false;
+
+            //  Act
+            bool actual = order.Validate();
+
+            //  Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
