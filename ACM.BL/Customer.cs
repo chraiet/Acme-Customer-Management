b068855ffda47a06007a2a0b08881f9afeafb63d
Momo <chraiet009@outlook.com>
2020-01-15 08:09:28 -0500

    Added relationships

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -8,7 +8,7 @@ namespace ACM.BL
 {
     public class Customer
     {
-        public Customer()
+        public Customer(): this(0)
         {
 
         }
@@ -16,9 +16,11 @@ namespace ACM.BL
         public Customer(int customerId)
         {
             CustomerId = customerId;
+            AddressList = new List<Address>();
         }
 
         public int CustomerId { get; private set; }
+        public int CustomerType { get; set; }
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -34,6 +36,7 @@ namespace ACM.BL
                     return $"{LastName}, {FirstName}";
             }
         }
+        public List<Address> AddressList { get; set; }
 
         public static int InstanceCount { get; set; }
 
